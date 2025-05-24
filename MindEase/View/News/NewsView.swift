a1ae61b7273accc19b004ce1f1a3b3d174e6f141
Ricky Primayuda Putra <rickyprima30@gmail.com>
2025-05-24 16:38:21 +0700

    feat: foow

--- MindEase/View/News/NewsView.swift
+++ MindEase/View/News/NewsView.swift
@@ -31,7 +31,7 @@ struct NewsView: View {
                         LazyVStack(spacing: 20) {
                             ForEach(newsViewModel.news.indices, id: \.self) { index in
                                 let article = newsViewModel.news[index]
-                                NewsCardView(news: article)
+                                NewsCard(news: article)
                                     .padding(.horizontal)
                             }
                         }
@@ -46,52 +46,5 @@ struct NewsView: View {
         .onAppear {
             newsViewModel.getNews()
         }
-    }
-}
-
-struct NewsCardView: View {
-    let news: News
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            if let imageUrl = news.urlToImage, let url = URL(string: imageUrl) {
-                WebImage(url: url)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: UIScreen.main.bounds.width - 50, height: 200)
-                    .clipped()
-                    .cornerRadius(10)
-            } else {
-                Rectangle()
-                    .fill(Color.gray.opacity(0.3))
-                    .frame(height: 200)
-                    .overlay(
-                        Text("Tidak ada gambar")
-                            .font(AppFont.Poppins.regular(16))
-                            .foregroundColor(.gray)
-                    )
-                    .cornerRadius(10)
-            }
-            
-            Text(news.title)
-                .font(AppFont.Poppins.bold(16))
-                .foregroundColor(.primary)
-            
-            if let desc = news.description {
-                Text(desc)
-                    .font(AppFont.Poppins.regular(14))
-                    .foregroundColor(.secondary)
-                    .lineLimit(3)
-            }
-            
-            Text("Sumber: \(news.source.name)")
-                .font(AppFont.Poppins.regular(12))
-                .foregroundColor(.gray)
-        }
-        .padding()
-        .background(Color.white.opacity(0.4))
-        .cornerRadius(15)
-        .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
-        .frame(width: UIScreen.main.bounds.width - 32, height: 350)
     }
 }
